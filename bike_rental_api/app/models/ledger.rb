3ca8346c1399babb44bbc70e9f4a99c3cdb21a68
Leo <hamlap6@gmail.com>
2020-07-09 00:30:35 -0700

    added validation for duplicate ledger

--- bike_rental_api/app/models/ledger.rb
+++ bike_rental_api/app/models/ledger.rb
@@ -1,4 +1,7 @@
 class Ledger < ApplicationRecord
   belongs_to :location
   belongs_to :bike
+
+  validates_uniqueness_of :bike_id, scope: :location_id
+  validates_uniqueness_of :location_id, scope: :bike_id
 end
